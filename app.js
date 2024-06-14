da2de51e35e2075dece86208959c32c8c5a4fe37
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-06-14 20:35:58 +0700

    instal nodemon & basic routing dengan express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+
+app.get('/', (req,res)=>{
+    res.send('<h2>Hello World</h2><hr>')
+})
+
+
+app.get('/profil', (req,res)=>{
+    res.send('<h1>Profil saya</h1>')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`App sudah siap, buka http://localhost:${port}`)
+})
