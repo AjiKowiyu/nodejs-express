d15892de0c8bafdfe257f856eac13903f33c97ba
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-06-20 09:45:17 +0700

    database modelling

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.get('/pengalaman', (req,res)=>{
 })
 
 
+app.get('/karyawan', async (req,res)=>{
+    const m_karyawan = require('./model/m_karyawan')
+    let dataview = {
+        semua_karyawan: await m_karyawan.get_semua_karyawan(),
+    }
+    res.render('karyawan/all', dataview)
+})
+
+
 app.listen(port, ()=>{
     console.log(`App sudah siap, buka http://localhost:${port}`)
 })
